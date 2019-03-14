341239e7855e81c0bd2b1405cea88ff673b442b3
Shashank Shekhar <shashank7200@gmail.com>
2019-03-15 00:54:27 +0530

    change the typos for the navbar labels

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -80,7 +80,7 @@ const Navbar = (props) => {
                     root: classes.tabRoot,
                     selected: classes.tabSelected
                   }}
-                  label="Animals"
+                  label="ANIMALS"
                 />
                 <Tab
                   disableRipple
@@ -88,7 +88,7 @@ const Navbar = (props) => {
                     root: classes.tabRoot,
                     selected: classes.tabSelected
                   }}
-                  label="Fruits & Veg"
+                  label="FRUITS & VEG"
                 />
               </Tabs>
             </Grid>
