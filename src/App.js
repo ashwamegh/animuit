ab24d4853478d6141cc2c2aa7880ed51931efa63
Shashank Shekhar <shashank7200@gmail.com>
2019-03-16 20:49:23 +0530

    created methods to fetch data from api, passed down as props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import "./App.css";
 import { Navbar, Main } from "./components";
 
-const styles = theme => ({
-  container: {
-    padding: "0 60px",
-    height: "100%"
-  }
-});
-
 class App extends Component {
+  state = {
+    items: [],
+    isLoading: true
+  };
+
+  fetchItems = endpoint => {
+    return new Promise((resolve, reject) => {
+      fetch(endpoint)
+        .then(response => {
+          if (response.status !== 200) {
+            reject(
+              "Looks like there was a problem. Status Code: " + response.status
+            );
+          }
+
+          // Examine the items in the response
+          response.json().then((data) => {
+            resolve(data);
+          });
+        })
+        .catch(function(err) {
+          reject("Fetch Error :-S", err);
+        });
+    });
+  };
+
+  updateItemsList = category => {
+    try {
+      switch (category) {
+        case "animals":
+          this.fetchItems("http://styleguide.effectivedigital.com/interview/api/animals")
+          .then((response) => console.log(response));
+          return;
+        case "fruits&veg":
+          this.fetchItems("http://styleguide.effectivedigital.com/interview/api/fruitveg")
+          .then((response) => console.log(response));
+          return;
+        default:
+          return;
+      }
+    } catch (error) {
+      
+    }
+    
+  };
+
   render() {
-    const { classes } = this.props;
+    const { isLoading, items } = this.state;
 
     return (
       <div>
-        <Navbar />
-        <Main />
+        <Navbar loadItems={this.updateItemsList}/>
+        <Main isLoading={isLoading} items={items} />
       </div>
     );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(App);
+export default App;
