ef39a0f4de6e48c1e8aaa8538a713447cdcbc800
Shashank Shekhar <shashank7200@gmail.com>
2019-03-17 16:30:32 +0530

    added navbar drawer for mobile devices

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import classNames from 'classnames';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
 import Grid from "@material-ui/core/Grid";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from '@material-ui/icons/Menu';
 import PhotoLibrary from "@material-ui/icons/PhotoLibraryRounded";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { withSize } from 'react-sizeme';
+
+const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
@@ -70,14 +82,53 @@ const styles = theme => ({
     }
   },
   tabSelected: {},
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  drawerToolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  drawerListItem: {
+    color: theme.palette.primary.main,
+    cursor: 'pointer',
+
+    "&:hover": {
+      color: theme.palette.primary.light,
+      opacity: 1
+    },
+    "&:focus": {
+      color: theme.palette.primary.light
+    }
+  },
   typography: {
     padding: theme.spacing.unit * 3
   }
 });
 
+
 const Navbar = props => {
-  const { classes, loadItems } = props;
+  const { classes, loadItems, size, theme } = props;
   const [value, setValue] = useState(0);
+  const [ open, setDrawerStatus ] = useState(false);
 
   return (
     <div className={classes.root}>
@@ -86,13 +137,13 @@ const Navbar = props => {
         position="fixed"
         color="default"
       >
-        <Toolbar>
+        <Toolbar classes={{root: (size.width <= 612) && classes.drawerToolbar}} disableGutters={!open}>
           <a href="/">
             <IconButton className={classes.logoButton}>
               <PhotoLibrary className={classes.logoButtonIcon} />
             </IconButton>
           </a>
-          <Grid
+          {size.width > 612 ?<Grid
             container
             className={classes.gridWrapper}
             spacing={24}
@@ -128,15 +179,51 @@ const Navbar = props => {
               </Tabs>
             </Grid>
           </Grid>
+          :<IconButton
+          color="primary"
+          aria-label="Open drawer"
+          onClick={() => setDrawerStatus(true)}
+          className={classNames(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+      }
         </Toolbar>
       </AppBar>
+
+      <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={() => setDrawerStatus(false)} color="primary">
+              {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+              <ListItem disableRipple onClick={() => loadItems("animals")}>
+                <ListItemText classes={{ primary: classes.drawerListItem  }} primary={"ANIMALS"}/>
+              </ListItem>
+              <ListItem disableRipple onClick={() => loadItems("fruits&veg")}>
+                <ListItemText classes={{ primary: classes.drawerListItem  }} primary={"FRUITS & VEG"} />
+              </ListItem>
+          </List>
+        </Drawer>
+ 
     </div>
   );
 };
 
 Navbar.propTypes = {
   classes: PropTypes.object.isRequired,
-  loadItems: PropTypes.func.isRequired
+  loadItems: PropTypes.func.isRequired,
+  size: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Navbar);
+export default withSize()(withStyles(styles, { withTheme: true })(Navbar));
