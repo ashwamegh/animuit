6d9099eb705ac7d2a38d8bb1e958038c33a1a861
Shashank Shekhar <shashank7200@gmail.com>
2019-03-17 17:02:09 +0530

    Updates

---
+++ precache-manifest.05b371c79f587a36136ac1a8cd7153c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "362619ab398a8cb0c985",
+    "url": "/static/js/main.3b83d197.chunk.js"
+  },
+  {
+    "revision": "e37d2e508acbee124682",
+    "url": "/static/js/2.1d51a3e7.chunk.js"
+  },
+  {
+    "revision": "362619ab398a8cb0c985",
+    "url": "/static/css/main.1f34f481.chunk.css"
+  },
+  {
+    "revision": "816575a1c52dac8dd9fcc8e9465dad74",
+    "url": "/index.html"
+  }
+];
