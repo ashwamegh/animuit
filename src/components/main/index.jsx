b7afa3888afee44c4b5a7077ee7ba1a77b16dbbb
Shashank Shekhar <shashank7200@gmail.com>
2019-03-17 15:08:57 +0530

    added image dialog for full size image modal

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import { withSize } from 'react-sizeme';
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { withSize } from "react-sizeme";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
 
 const styles = theme => ({
   root: {
@@ -15,75 +17,129 @@ const styles = theme => ({
     minHeight: 400,
     flexDirection: "column",
     alignItems: "center",
-    margin: '0 auto',
+    margin: "0 auto",
     marginTop: 90,
     paddingBottom: 24,
-    '@media only screen and (max-width: 767px)':{
+    "@media only screen and (max-width: 767px)": {
       width: "90%"
-    },
+    }
+  },
+  dialogContent: {
+    padding: 0,
+    "&:first-child": {
+      padding: 0,
+      overflow: "hidden",
+      marginBottom: -12
+    }
+  },
+  dialogPaper: {
+    width: "auto",
+    margin: 24,
+    maxWidth: "90%",
+    animation: 'zoom 0.6s',
+  },
+  dialogImage: {
+    width: "100%"
   },
   listWrapper: {
-    width: '100%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
   },
   gridList: {
-    width: '100%',
-    height: '100%',
-    transform: 'translateZ(0)',
+    width: "100%",
+    height: "100%",
+    transform: "translateZ(0)"
   },
   gridListTile: {
-    animation: 'fadeIn 1s ease-in both',
+    animation: "fadeIn 1s ease-in both"
   },
   gridListTileInner: {
-    borderRadius: 0,
+    borderRadius: 0
   },
-  '@keyframes fadeIn': {
+  '@keyframes zoom': {
     'from': {
-      opacity: 0,
-      transform: 'translate3d(0, -20%, 0)',
+      transform: 'scale(0)'
     },
-    'to' : {
+    'to': {
+      transform: 'scale(1)'
+    }
+  },
+
+  "@keyframes fadeIn": {
+    from: {
+      opacity: 0,
+      transform: "translate3d(0, -20%, 0)"
+    },
+    to: {
       opacity: 1,
-      transform:' translate3d(0, 0, 0)',
+      transform: " translate3d(0, 0, 0)"
     }
   }
 });
 
 const Main = props => {
   const { classes, isLoading, items, size } = props;
+  const [ selectedImage, updateSelectedImage ] = useState("");
+  const [ open, setDialogStatus ] = useState(false);
   const deviceWidth = size.width;
-  let cols=3;
+  let cols = 3;
 
-  if(deviceWidth >= 1192){
+  if (deviceWidth >= 1192) {
     cols = 5;
-  }else if(deviceWidth >= 767){
+  } else if (deviceWidth >= 767) {
     cols = 4;
-  }else if(deviceWidth >= 512){
+  } else if (deviceWidth >= 612) {
     cols = 3;
-  }else if (deviceWidth >= 392){
+  } else if (deviceWidth >= 392) {
     cols = 2;
-  }else if(deviceWidth < 392){
+  } else if (deviceWidth < 392) {
     cols = 1;
   }
 
   return (
     <main className={classes.root}>
-      {isLoading?
-        <CircularProgress disableShrink />:
+      <Dialog
+        classes={{ paper: classes.dialogPaper }}
+        onClose={() => setDialogStatus(false)}
+        open={open}
+      >
+        <DialogContent className={classes.dialogContent}>
+          <img className={classes.dialogImage} src={selectedImage} />
+        </DialogContent>
+      </Dialog>
+      {isLoading ? (
+        <CircularProgress disableShrink />
+      ) : (
         <div className={classes.listWrapper}>
-        <GridList id="masonry" cellHeight={160} className={classes.gridList} cols={cols}>
-          {items.map(tile => (
-            <GridListTile classes={{ root: classes.gridListTile, tile: classes.gridListTileInner}} key={tile.Id} cols={tile.cols || 1}>
-              <img src={tile.ImageURLs.Thumb} alt={tile.Title} />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-    }
+          <GridList
+            id="masonry"
+            cellHeight={160}
+            className={classes.gridList}
+            cols={cols}
+          >
+            {items.map(tile => (
+              <GridListTile
+                classes={{
+                  root: classes.gridListTile,
+                  tile: classes.gridListTileInner
+                }}
+                key={tile.Id}
+                cols={tile.cols || 1}
+                onClick={() => {
+                  updateSelectedImage(tile.ImageURLs.FullSize);
+                  setDialogStatus(true);
+                }}
+              >
+                <img src={tile.ImageURLs.FullSize} alt={tile.Title} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      )}
     </main>
   );
 };
@@ -92,7 +148,7 @@ Main.propTypes = {
   classes: PropTypes.object.isRequired,
   isLoading: PropTypes.bool.isRequired,
   items: PropTypes.array.isRequired,
-  size: PropTypes.number.isRequired,
-}
+  size: PropTypes.object.isRequired,
+};
 
 export default withSize()(withStyles(styles)(Main));
